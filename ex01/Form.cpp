c68e361baaff9a2a887ff1d5af3fa74b22fff8ad
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2025-01-09 11:36:32 +0100

    Makefile funcionando

---
+++ ex01/Form.cpp
@@ -0,0 +1,6 @@
+# include "Form.hpp"
+
+std::string printMessage()
+{
+	return "Hello World!";
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,8 @@
+# include "Form.hpp"
+
+int main()
+{
+	std::string message = printMessage();
+
+	std::cout << message << std::endl;
+}
